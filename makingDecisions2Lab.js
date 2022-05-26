b7606c5c163735c887b3c199ac849f2278259834
Sam Bischoff <sam.bischoff2001@gmail.com>
2022-05-26 15:11:28 -0600

    completed lab

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -159,7 +159,23 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 
 //Code Here
 
-
+switch(letterGrade){
+  case 'A':
+  console.log("The student is doing excellently.")
+  break;
+  case 'B':
+  console.log("The student is doing well.")
+  break;
+  case 'C':
+  console.log("The student is doing alright.")
+  break;
+  case 'D':
+  console.log("The student is not doing very well.")
+  break;
+  case 'F':
+  console.log("The student is failing.")
+  break;
+}
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
@@ -186,3 +202,9 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+// for (let i = 1; i <= 100; i++){
+//   if (i % 15 === 0) console.log("DevMountain");
+//   else if (i % 3 === 0) console.log('Dev');
+//   else if (i % 5 === 0) console.log('Mountain');
+//   else console.log(i);
+// }
